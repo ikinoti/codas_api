29870c59f380bc537131c6e90d3967b1489a6d2d
ikinoti <gitongakinoti1@gmail.com>
2024-07-09 02:09:24 +0300

    views for the project

--- base_app/views.py
+++ base_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET', 'POST'])
+def endpoints(request):
+    data = ['/advocates', 'advocates/:username']
+    return Response(data)
+
+@api_view(['GET'])
+def advocate_list(request):
+    data = ['Kinoti', 'Cynthia', 'Kinya']
+    return Response(data)
+
+@api_view(['GET'])
+def advocate_detail(request, username):
+    data = username
+    return Response(data)
