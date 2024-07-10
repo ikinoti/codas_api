b1759cb41f290c0747b54bb589be7de65c1f6378
ikinoti <gitongakinoti1@gmail.com>
2024-07-11 01:18:11 +0300

    added search functionality

--- base_app/views.py
+++ base_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.db.models import Q
 
 from .models import Advocate
 from .serializers import AdvocateSerializer
@@ -14,7 +15,13 @@ def endpoints(request):
 
 @api_view(['GET'])
 def advocate_list(request):
-    advocate = Advocate.objects.all()
+    query = request.GET.get('query')
+
+    if query == None:
+        query = ''
+
+    # advocate = Advocate.objects.all()
+    advocate = Advocate.objects.filter(Q(username__icontains=query) | Q(bio__icontains=query))
     serializer = AdvocateSerializer(advocate, many=True)
     return Response(serializer.data)
 
