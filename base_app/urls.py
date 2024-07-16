1e913b1f9bfd8783563e240d2a663286f9348a6e
ikinoti <gitongakinoti1@gmail.com>
2024-07-16 23:43:13 +0300

    end point to generate token for authentication

--- base_app/urls.py
+++ base_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
 from . import views
 
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+
     path('', views.endpoints),
     path('advocates/', views.advocate_list),
     path('advocates/<str:username>', views.advocate_detail),
